9b74c2bc49ffbd1cb4fb82862b7c86d440abc742
jkluio132465 <jkluio132465@gmail.com>
2017-10-25 00:26:38 +0800

    設定路由

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',['as'=>'home.index','uses'=>
+'HomeController@index']);
+
+
+Route::get('/about',['as'=>'about.index','uses'=>
+'HomeController@about']);
+
+Route::get('/news',['as'=>'news.index','uses'=>
+'HomeController@news']);
+
